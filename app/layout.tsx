fc59894079c7890f9a09b5c15da528ca57dea208
Vladimir Davtyan <vladdavtyan437@gmail.com>
2024-11-22 19:17:25 +0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,36 +5,35 @@ import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "next-themes";
 
-
 const fontSans = FontSans({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: "CarePulse",
-  description:
-    "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
+    title: "CarePulse",
+    description:
+        "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-dark-300 font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning className="dark">
+            <body
+                className={cn(
+                    "min-h-screen bg-dark-300 font-sans antialiased",
+                    fontSans.variable
+                )}
+            >
+                <ThemeProvider attribute="class" defaultTheme="dark">
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
